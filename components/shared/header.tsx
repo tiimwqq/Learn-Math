e3150339b8275949c8aeb01ead45670298f12879
unknown <tiimwqq@gmail.com>
2024-12-09 22:00:56 +0500

    refactor(header): обновлен компонент с добавлением навигации, поиска и ссылки на главную
    - Добавлен компонент Navigation для организации навигации по сайту
    - Вставлен элемент поиска с иконкой поиска и полем ввода
    - Обернут заголовок в ссылку на главную страницу для навигации
    - Добавлен компонент Input для поиска
    - Использованы компоненты Container, Button и Input для стилизации

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,8 +1,10 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { Container } from './container';
-import { Button } from '../ui';
-import { User } from 'lucide-react';
+import { Button, Input } from '../ui';
+import { Search, User } from 'lucide-react';
+import { Navigation } from './navigation';
+import Link from 'next/link';
 
 interface Props {
 	className?: string;
@@ -13,12 +15,21 @@ export const Header: React.FC<Props> = ({ className }) => {
 		<header className={cn('border border-b', className)}>
 			<Container className='flex items-center justify-between py-8'>
 				{/* левая часть */}
-				<div className='flex items-center gap-4'>
-					<h1 className='text-2xl font-black'>Learn Math</h1>
+				<Link href={'/'}>
+					<div className='flex items-center gap-4'>
+						<h1 className='text-2xl font-black'>Learn Math</h1>
+					</div>
+				</Link >
+				{/* навигация и поиск */}
+				<Navigation />
+				<div className={cn('w-1/3 relative ')}>
+					<Search className={cn('absolute top-2.5 left-3 divide-gray-500')} size={16} />
+					<Input className={cn('pl-9')} placeholder='Поиск' />
 				</div>
+				{/* кнопки */}
 				<div className='flex items-center gap-3'>
 					<Button variant='outline' className='flex items-center gap-3'>
-						<User size={16}/>
+						<User size={16} />
 						Войти</Button>
 				</div>
 			</Container>

d24efa23f3d4bdba5b0b7e53ff91e210eea74170
unknown <tiimwqq@gmail.com>
2025-01-14 22:31:49 +0500

    feat(project-structure): updated page.tsx in app

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,22 @@ import { Container, Title } from "@/components/shared";
 import img from '../public/images/Octahedron.svg'
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { TopicsOverview } from "@/components/topicsOverview";
 
 export default function Home() {
 	return (
-		<Container className={cn('flex justify-between')}>
-			<div>
-				<Title text="Математика от А до Я" size="xl" className="font-extrabold mt-20" />
-				<p className="text-sm text-gray-400 leading-3">Учите математику вместе с нами.</p>
+		<Container>
+			<div className={cn('flex justify-between')}>
+				<div>
+					<Title text="Математика от А до Я" size="xl" className="font-extrabold mt-20" />
+					<p className="text-sm text-gray-400 leading-3">Учите математику вместе с нами.</p>
+				</div>
+				<Image src={img} alt="piramid" className={cn('mt-28')} />
 			</div>
-			<Image src={img} alt="piramid" className={cn('mt-28')} />
+			<TopicsOverview />
 		</Container >
 	);
 }
+
+
+

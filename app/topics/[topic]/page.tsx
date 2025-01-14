9ca5e48478166c62e447a0067749b0566fcc0f1b
unknown <tiimwqq@gmail.com>
2025-01-14 22:28:30 +0500

    feat(project-structure): added topics folder with [topic] subdirectory and page.tsx file
    - created topics folder
    - added [topic] subdirectory inside topics folder
    - created page.tsx file inside [topic] subdirectory

---
+++ app/topics/[topic]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+	num: number
+};
+
+const MyComponent: React.FC<Props> = ({ }) => {
+	return (
+		<div>
+			MyComponent
+		</div>
+	);
+};
+
+export default MyComponent;
